bd68a9747028297297784de24580d30f359f8631
Shun Takebayashi <shun@takebayashi.asia>
2016-03-13 19:04:41 +0900

    Update to use Inquiline.Request.content

--- Server/application.swift
+++ Server/application.swift
@@ -5,17 +5,8 @@ import Nest
 public func application(request: RequestType) -> ResponseType {
   switch request.method {
   case "POST":
-    if var payload = request.body {
-      var buffer = [CChar]()
-      while let chunk = payload.next() {
-        buffer.appendContentsOf(chunk.map({ CChar($0) }))
-      }
-      buffer.append(0)
-      if let body = String.fromCString(buffer) {
-        return Response(.Ok, contentType: "text/plain", content: "Hello " + body)
-      } else {
-        return Response(.BadRequest, contentType: "text/plain", content: "Request body was broken")
-      }
+    if let content = request.content {
+      return Response(.Ok, contentType: "text/plain", content: "Hello " + content)
     } else {
       return Response(.NotImplemented, contentType: "text/plain", content: "Request body was empty")
     }
